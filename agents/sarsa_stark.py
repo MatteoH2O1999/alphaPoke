a28a5c620e48087f7465046ca247cf99d1fd80a1
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-01-21 16:53:00 +0100

    Add sarsa interface

--- agents/sarsa_stark.py
+++ agents/sarsa_stark.py
@@ -1,23 +1,31 @@
-# AI trained with SARSA algorithm
-from poke_env.environment.abstract_battle import AbstractBattle
-from poke_env.player.battle_order import BattleOrder
-from poke_env.player.player import Player
-
-from . import LEARNING_RATE_WHILE_PLAYING
+# AI trained with SARSA or Q-learning algorithm
+from .basic_rl import SimpleRLAgent
+from .expert_rl import ExpertRLAgent
 
 
-class SarsaStark(Player):
+class SarsaStark(SimpleRLAgent):
 
-    def __init__(self, **kwargs):
-        self.model = kwargs.get('model', {})
-        self.training = kwargs.get('training', False)
-        self.train_while_playing = kwargs.get('keep_training', False)
-        self.static_epsilon = LEARNING_RATE_WHILE_PLAYING
-        if self.training:
-            self.training_steps = 0
-            self.last_state = None
-            self.last_action = None
-        super().__init__(**kwargs)
+    def _train(self, last_state, last_action, reward):
+        # TODO
+        pass
 
-    def choose_move(self, battle: AbstractBattle) -> BattleOrder:
+
+class ExpertSarsaStark(ExpertRLAgent):
+
+    def _train(self, last_state, last_action, reward):
+        # TODO
+        pass
+
+
+class QSarsaStark(SimpleRLAgent):
+
+    def _train(self, last_state, last_action, reward):
+        # TODO
+        pass
+
+
+class QExpertSarsaStark(ExpertRLAgent):
+
+    def _train(self, last_state, last_action, reward):
+        # TODO
         pass
