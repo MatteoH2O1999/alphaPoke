e847707a756aa56794689a59c4c61616978f6a11
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-03-13 20:49:34 +0100

    Update description of the baselines

--- gui_data.py
+++ gui_data.py
@@ -1,6 +1,15 @@
-DAD_DESCRIPTION = "dad description"
-EIGHT_YEAR_OLD_ME_DESCRIPTION = "8 year old me description"
-TWENTY_YEAR_OLD_ME_DESCRIPTION = "20 year old me description"
+DAD_DESCRIPTION = (
+    "A random player that chooses every action randomly from all the available ones. "
+    "A player of this type is the weakest baseline without actively trying to lose."
+)
+EIGHT_YEAR_OLD_ME_DESCRIPTION = (
+    "A player that uses the move with the highest base power, completely ignoring "
+    "typing and additional effects. If it cannot attack, it will perform a random action "
+    "(including switches)."
+)
+TWENTY_YEAR_OLD_ME_DESCRIPTION = (
+    "A player that implements a basic strategy using simple heuristics."
+)
 SIMPLE_RL_DESCRIPTION = "simple rl description"
 EXPERT_RL_DESCRIPTION = "expert rl description"
 SIMPLE_SARSA_DESCRIPTION = "simple sarsa description"
@@ -15,39 +24,19 @@ SIMPLE_SARSA_SUPPORTED_BATTLES = {"[Gen 8] Random Battle": "gen8randombattle"}
 EXPERT_SARSA_SUPPORTED_BATTLES = {"[Gen 8] Random Battle": "gen8randombattle"}
 
 PLAYER_TYPE_DICT = {
-    "Dad": (
+    "Random Player": (
         "dad",
         DAD_DESCRIPTION,
         DAD_SUPPORTED_BATTLES,
     ),
-    "8-Year-Old me": (
+    "Max Base Power Player": (
         "8-year-old-me",
         EIGHT_YEAR_OLD_ME_DESCRIPTION,
         EIGHT_YEAR_OLD_ME_SUPPORTED_BATTLES,
     ),
-    "20-Year-Old me": (
+    "Simple Heuristics Player": (
         "20-year-old-me",
         TWENTY_YEAR_OLD_ME_DESCRIPTION,
         TWENTY_YEAR_OLD_ME_SUPPORTED_BATTLES,
-    ),
-    "Simple Reinforcement Learning Player": (
-        "simpleRL-best",
-        SIMPLE_RL_DESCRIPTION,
-        SIMPLE_RL_SUPPORTED_BATTLES,
-    ),
-    "Expert Reinforcement Learning Player": (
-        "expertRL-best",
-        EXPERT_RL_DESCRIPTION,
-        EXPERT_RL_SUPPORTED_BATTLES,
-    ),
-    "Sarsa Trained Player": (
-        "simpleSarsaStark-best",
-        SIMPLE_SARSA_DESCRIPTION,
-        SIMPLE_SARSA_SUPPORTED_BATTLES,
-    ),
-    "Expert Sarsa Trained Player": (
-        "expertSarsaStark-best",
-        EXPERT_SARSA_DESCRIPTION,
-        EXPERT_SARSA_SUPPORTED_BATTLES,
     ),
 }
