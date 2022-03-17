32b9697cd8b63e0f1857933c615d3e5b772ad56a
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-03-17 17:02:42 +0100

    Fix evaluation integration test to not create files and to avoid tkinter

--- tests/integration_tests/test_integration_eval.py
+++ tests/integration_tests/test_integration_eval.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pytest
 
 from unittest.mock import patch
@@ -9,5 +10,10 @@ from eval import main
 @pytest.mark.timeout(90)
 @pytest.mark.flaky(max_runs=10, min_passes=1)
 async def test_eval_integration():
-    with patch("sys.argv", ["", "300", "gen8randombattle", "dad"]):
+    with patch("sys.argv", ["", "300", "gen8randombattle", "dad"]), patch(
+        "matplotlib.pyplot.savefig"
+    ) as mock_savefig, patch("os.makedirs") as mock_makedirs:
+        plt.switch_backend("agg")
         await main()
+        mock_savefig.assert_called_once()
+        mock_makedirs.assert_called_once_with("./logs", exist_ok=True)
