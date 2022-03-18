da25aecdb0828e834f2bb895ec44f02556951295
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-03-18 16:24:22 +0100

    Update rank.py to reset account elo

--- rank.py
+++ rank.py
@@ -2,16 +2,25 @@
 import asyncio
 import datetime
 import getpass
+import json
 import math
 import matplotlib.pyplot as plt
 import multiprocessing
 import os
+import requests
 import seaborn as sns
 import sys
+import time
 
+from csv import DictReader
 from numpy import mean
+from poke_env.environment.abstract_battle import AbstractBattle
+from poke_env.player.battle_order import ForfeitBattleOrder, BattleOrder
 from poke_env.server_configuration import ShowdownServerConfiguration
+from poke_env.player.player import Player
 from poke_env.player_configuration import PlayerConfiguration
+from poke_env.utils import to_id_str
+from typing import Awaitable, Union
 
 from agents.base_classes.trainable_player import TrainablePlayer
 from utils.create_agent import create_agent
@@ -64,6 +73,20 @@ def main():
             cont.value = 0
     for p in processes:
         p.join()
+    complete_data = []
+    for file in os.listdir(save_path):
+        if ".csv" in file:
+            with open(os.path.join(save_path, file)) as agent_file:
+                reader = DictReader(agent_file, delimiter=";")
+                for row in reader:
+                    complete_data.append(
+                        (file.replace(".csv", ""), row["index_of_battle"], row["elo"])
+                    )
+            os.remove(os.path.join(save_path, file))
+    with open(os.path.join(save_path, "data.csv"), "w") as data_file:
+        data_file.write("agent;index_of_battle;elo")
+        for data in complete_data:
+            data_file.write(f"{data[0]};{data[1]};{data[2]}")
 
 
 class PlayerProcess(multiprocessing.Process):
@@ -95,6 +118,7 @@ class PlayerProcess(multiprocessing.Process):
         self.stop_on = 1
 
     def run(self) -> None:
+        self.reset_elo()
         plt.switch_backend("agg")
         self.agent = create_agent(
             self.agent_type,
@@ -109,16 +133,9 @@ class PlayerProcess(multiprocessing.Process):
         elo_stats = [[0], [0]]
         while self.cont or self.stop_on >= self.count:
             asyncio.get_event_loop().run_until_complete(self.agent.ladder(1))
+            time.sleep(10)
             elo_stats[0].append(self.count)
-            battles = self.agent.battles
-            if len(battles) != 1:
-                raise RuntimeError("???")
-            for b in battles.values():
-                if b.rating:
-                    elo_stats[1].append(b.rating)
-                else:
-                    elo_stats[1].append(elo_stats[1][-1])
-            self.agent.reset_battles()
+            elo_stats[1].append(get_ratings(self.username, self.battle_format)['elo'])
             with self.stop_on_shared.get_lock():
                 if self.stop_on_shared.value < self.count:
                     self.stop_on_shared.value = self.count
@@ -137,6 +154,12 @@ class PlayerProcess(multiprocessing.Process):
             self.agent.training or self.agent.train_while_playing
         ):
             update_model(self.agent, "./models")
+        with open(
+            os.path.join(self.save_path, f"{self.agent.__class__.__name__}.csv"), "w"
+        ) as data_file:
+            data_file.write("index_of_battle;elo")
+            for index_of_battle, elo in zip(elo_stats[0], elo_stats[1]):
+                data_file.write(f"{index_of_battle};{elo}")
         if len(elo_stats[1]) > 1:
             elo_stats[0] = elo_stats[0][1:]
             elo_stats[1] = elo_stats[1][1:]
@@ -176,6 +199,52 @@ class PlayerProcess(multiprocessing.Process):
         if len(elo_stats[0]) < 10:
             plt.gca().tick_params(axis="x", label1On=False)
         plt.savefig(self.plot_path, backend="agg", bbox_inches="tight")
+
+    def reset_elo(self):
+        p = ResetProcess(self.username, self.password, self.battle_format)
+        p.start()
+        p.join()
+
+
+class ResetPlayer(Player):
+
+    def choose_move(
+        self, battle: AbstractBattle
+    ) -> Union[BattleOrder, Awaitable[BattleOrder]]:
+        return ForfeitBattleOrder()
+
+
+class ResetProcess(multiprocessing.Process):
+    def __init__(self, username, password, battle_format, *args, **kwargs):
+        self.username = username
+        self.password = password
+        self.battle_format = battle_format
+        super().__init__(*args, **kwargs)
+
+    def run(self) -> None:
+        player_config = PlayerConfiguration(self.username, self.password)
+        agent = ResetPlayer(
+            player_configuration=player_config,
+            battle_format=self.battle_format,
+            server_configuration=ShowdownServerConfiguration,
+        )
+        current_elo = get_ratings(self.username, self.battle_format)['elo']
+        while current_elo != 1000:
+            asyncio.get_event_loop().run_until_complete(agent.ladder(1))
+            time.sleep(10)
+            current_elo = get_ratings(self.username, self.battle_format)['elo']
+
+
+def get_ratings(username, battle_format):
+    json_data = requests.get(f'https://pokemonshowdown.com/users/{to_id_str(username)}.json')
+    data = json.loads(json_data.content)
+    rating_data = data['ratings'][battle_format]
+    for key, value in rating_data.items():
+        if key == 'elo':
+            rating_data[key] = int(value)
+        else:
+            rating_data[key] = float(value)
+    return rating_data
 
 
 if __name__ == "__main__":  # pragma: no cover
