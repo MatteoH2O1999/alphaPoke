65280868b127edb32a28362f652755f025effce2
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-03-22 10:30:16 +0100

    Add generic action_to_move function

--- utils/action_to_move_function.py
+++ utils/action_to_move_function.py
@@ -1,9 +1,35 @@
 # Return a function with the correct battle format from AI action space to showdown env.
-from poke_env.environment.battle import Battle
+from poke_env.environment.abstract_battle import AbstractBattle
 from poke_env.player.battle_order import BattleOrder, ForfeitBattleOrder
+from poke_env.player.player import Player
+from typing import Callable
 
 
-def action_to_move_gen8single(agent, action: int, battle: Battle) -> BattleOrder:
+def get_int_action_to_move(
+    battle_format: str, is_double: bool
+) -> Callable[[Player, int, AbstractBattle], BattleOrder]:
+    if is_double:
+        raise NotImplementedError("Double battles are not yet implemented.")
+    else:
+        if "gen8" in battle_format:
+            return action_to_move_gen8single
+        else:
+            raise NotImplementedError(f"{battle_format} is not yet implemented.")
+
+
+def get_int_action_space_size(battle_format: str, is_double: bool) -> int:
+    if is_double:
+        raise NotImplementedError("Double battles are not yet implemented.")
+    else:
+        if "gen8" in battle_format:
+            return 22
+        else:
+            raise NotImplementedError(f"{battle_format} is not yet implemented.")
+
+
+def action_to_move_gen8single(
+    agent: Player, action: int, battle: AbstractBattle
+) -> BattleOrder:
     if action == -1:
         return ForfeitBattleOrder()
     elif (
