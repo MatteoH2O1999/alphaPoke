ae4575457aaf82858ca6080c36d7041ee1d64aa8
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-03-24 18:53:16 +0100

    Add server update check to launcher scripts

--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,6 @@
 @echo off
 cd pokemon-showdown
+git pull
+call npm install
+echo 0 > logs/lastbattle.txt
 node pokemon-showdown start --no-security
--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,5 @@
 cd pokemon-showdown
+git pull
+npm install
+printf 0 > logs/lastbattle.txt
 node pokemon-showdown start --no-security
