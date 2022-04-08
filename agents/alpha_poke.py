0a018402fcad1e19c14648961e71d6ea0fe71dc5
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-04-08 15:30:17 +0200

    Continue implementation of AlphaPoke

--- agents/alpha_poke.py
+++ agents/alpha_poke.py
@@ -1,6 +1,6 @@
 # Module containing production-level agents with neural networks
 from abc import ABC
-from gym import Space
+from gym.spaces import Space
 from poke_env.environment.abstract_battle import AbstractBattle
 from poke_env.player.baselines import (
     RandomPlayer,
@@ -9,6 +9,7 @@ from poke_env.player.baselines import (
 )
 from poke_env.player.openai_api import ObservationType
 from poke_env.player.player import Player
+from poke_env.player.utils import background_evaluate_player
 from tf_agents.agents import TFAgent
 from tf_agents.agents.tf_agent import LossInfo
 from tf_agents.drivers.py_driver import PyDriver
@@ -28,6 +29,11 @@ rewards = {
 
 
 class AlphaPokeEmbedded(DQNPlayer, ABC):
+    def __init__(self, log_interval=1000, eval_interval=10_000, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.log_int = log_interval
+        self.eval_int = eval_interval
+
     def calc_reward(
         self, last_battle: AbstractBattle, current_battle: AbstractBattle
     ) -> float:
@@ -58,6 +64,41 @@ class AlphaPokeEmbedded(DQNPlayer, ABC):
         ]
         return [cls(battle_format=self.battle_format) for cls in opponents_classes]
 
+    @property
+    def log_interval(self) -> int:
+        return self.log_int
+
+    @property
+    def eval_interval(self) -> int:
+        return self.eval_int
+
+    def eval_function(self, step):
+        num_challenges = 2000
+        placement_challenges = 40
+
+        eval_env, agent = self.create_evaluation_env(active=False)
+        policy = self.agent.policy
+        task = background_evaluate_player(
+            agent, n_battles=num_challenges, n_placement_battles=placement_challenges
+        )
+        for _ in range(num_challenges):
+            time_step = eval_env.reset()
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = eval_env.step(action_step)
+        evaluation = task.result()
+
+        if "evaluations" not in self.evaluations.keys():
+            self.evaluations["evaluations"] = [[], []]
+        self.evaluations["evaluations"][0].append(step)
+        self.evaluations["evaluations"][1].append(evaluation)
+
+    def log_function(self, step, loss_info: LossInfo):
+        if "losses" not in self.evaluations.keys():
+            self.evaluations["losses"] = [[], []]
+        self.evaluations["losses"][0].append(step)
+        self.evaluations["losses"][1].append(loss_info.loss)
+
 
 class AlphaPokeDQN(AlphaPokeEmbedded):
     def get_agent(self) -> TFAgent:
@@ -70,18 +111,4 @@ class AlphaPokeDQN(AlphaPokeEmbedded):
         pass
 
     def get_collect_driver(self) -> PyDriver:
-        pass
-
-    @property
-    def log_interval(self) -> int:
-        return 1000
-
-    @property
-    def eval_interval(self) -> int:
-        return 10_000
-
-    def eval_function(self, step):
-        pass
-
-    def log_function(self, step, loss_info: LossInfo):
         pass
