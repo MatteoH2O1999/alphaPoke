50c76e64ca9c3b159a31d5ec1ef104a0554fb3d2
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-04-14 21:50:58 +0200

    Add tests for new utils functions

--- tests/unit_tests/utils/test_get_smogon_data.py
+++ tests/unit_tests/utils/test_get_smogon_data.py
@@ -1,6 +1,7 @@
-from unittest.mock import MagicMock, PropertyMock, patch
+from poke_env.data import GEN_TO_POKEDEX
+from unittest.mock import MagicMock, patch
 
-from utils.get_smogon_data import get_random_battle_learnset
+from utils.get_smogon_data import get_random_battle_learnset, get_items, get_abilities
 
 
 def test_get_random_battle_learnset_gen8():
@@ -38,3 +39,56 @@ def test_get_random_battle_learnset_gen6():
         assert learnset["gastrodoneast"]["items"] == []
         assert learnset["zygarde"]["items"] == ["item"]
         assert learnset["zygardecomplete"]["items"] == ["item"]
+
+
+def get_fake_dict():
+    return {
+        6: {
+            "Pokemon 1": {"abilities": {"H": "Ability 1", "1": "Ability 2"}},
+            "Pokemon 2": {"abilities": {"H": "Ability 2", "1": "Ability 3"}},
+        },
+        8: {
+            "Pokemon 1": {"abilities": {"H": "Ability 2", "1": "Ability 3"}},
+            "Pokemon 2": {
+                "abilities": {"H": "Ability 3", "1": "Ability 4", "2": "Ability 5"}
+            },
+        },
+    }
+
+
+def test_get_abilities_gen6():
+    tmp = GEN_TO_POKEDEX.copy()
+    GEN_TO_POKEDEX.clear()
+    GEN_TO_POKEDEX.update(get_fake_dict())
+    abilities = list([v.name for v in get_abilities(6)])
+    assert len(get_abilities(6)) == 3
+    assert abilities == ["ability1", "ability2", "ability3"]
+    GEN_TO_POKEDEX.clear()
+    GEN_TO_POKEDEX.update(tmp)
+
+
+def test_get_abilities_gen8():
+    tmp = GEN_TO_POKEDEX.copy()
+    GEN_TO_POKEDEX.clear()
+    GEN_TO_POKEDEX.update(get_fake_dict())
+    abilities = list([v.name for v in get_abilities(8)])
+    assert len(get_abilities(8)) == 4
+    assert abilities == ["ability2", "ability3", "ability4", "ability5"]
+    GEN_TO_POKEDEX.clear()
+    GEN_TO_POKEDEX.update(tmp)
+
+
+def get_fake_json():
+    return {"item1": None, "item2": None, "Item3": None}
+
+
+def test_get_items():
+    with patch("requests.get") as mock_req_get, patch("json5.loads") as mock_loads:
+        data = MagicMock()
+        mock_req_get.return_value = data
+        mock_loads.return_value = get_fake_json()
+        items = get_items()
+        assert len(items) == 3
+        assert [i.name for i in items] == ["item1", "item2", "item3"]
+        mock_req_get.assert_called_once()
+        mock_loads.assert_called_once_with(data.content)
