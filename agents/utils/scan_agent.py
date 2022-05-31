f0e17cd59b804424749b4f96d52d375d69148cf9
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-05-31 17:42:12 +0200

    Remove lru_cache from scan_agent

--- agents/utils/scan_agent.py
+++ agents/utils/scan_agent.py
@@ -1,7 +1,6 @@
 # Simple agent to analyze battle information
 import asyncio
 
-from functools import lru_cache
 from poke_env.environment.abstract_battle import AbstractBattle
 from poke_env.player.battle_order import BattleOrder
 from poke_env.player.baselines import RandomPlayer
@@ -20,7 +19,6 @@ class ScanAgent(Player):
         action_to_move_func = self.get_action_to_move_func()
         return action_to_move_func(self, int(action), battle, False)
 
-    @lru_cache(1)
     def get_action_to_move_func(self):
         return get_int_action_to_move(self.format, self.format_is_doubles)
 
