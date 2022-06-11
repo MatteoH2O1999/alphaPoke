ff455b32727fc36f34d5ebd2fef191d1f176405b
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-06-11 14:24:53 +0200

    Fix macos sed

---
+++ .github/scripts/check_server.py
@@ -0,0 +1,49 @@
+import asyncio
+from typing import Awaitable, Union
+
+from poke_env.server_configuration import LocalhostServerConfiguration
+from poke_env.environment.abstract_battle import AbstractBattle
+from poke_env.player.battle_order import ForfeitBattleOrder, BattleOrder
+from poke_env.player.random_player import RandomPlayer
+from poke_env.player.player import Player
+
+from utils.close_player import close_player
+
+
+class ResetPlayer(Player):
+    def choose_move(
+        self, battle: AbstractBattle
+    ) -> Union[BattleOrder, Awaitable[BattleOrder]]:
+        return ForfeitBattleOrder()
+
+
+async def main():
+    print("Instantiating players...")
+    player = ResetPlayer(
+        server_configuration=LocalhostServerConfiguration,
+        battle_format="gen8randombattle",
+    )
+    opponent = RandomPlayer(
+        server_configuration=LocalhostServerConfiguration,
+        battle_format="gen8randombattle",
+    )
+    print("Trying battle...")
+    await player.battle_against(opponent, 1)
+    print("Stopping players...")
+    close_player(player)
+    close_player(opponent)
+
+
+if __name__ == "__main__":
+    print("Waiting for showdown server...")
+    redo = True
+    while redo:
+        redo = False
+        try:
+            print("Trying test battle...")
+            asyncio.new_event_loop().run_until_complete(asyncio.wait_for(main(), 10))
+            print("Test battle successful!!!")
+        except asyncio.TimeoutError:
+            print("Test battle timed out...")
+            redo = True
+            print("Retrying...")
--- run_server.sh
+++ run_server.sh
@@ -1,7 +1,7 @@
 git submodule update --remote
 cd pokemon-showdown
 cp config/config-example.js config/config.js
-sed -i 's/exports.repl = true;/exports.repl = false;/' config/config.js
+sed -i 's/exports.repl = true;/exports.repl = false;/' ./config/config.js
 npm install
 printf 0 > logs/lastbattle.txt
 node pokemon-showdown start --no-security
