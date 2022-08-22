c3b70efaacfccca2fef1d7c578c0c4425c1905e7
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-08-22 20:55:02 +0200

    Catch SSLError with requests

--- utils/get_player_info.py
+++ utils/get_player_info.py
@@ -1,13 +1,24 @@
 import json
+import time
 import requests
 
 from poke_env.utils import to_id_str
 
 
 def get_ratings(username, battle_format):
-    json_data = requests.get(
-        f"https://pokemonshowdown.com/users/{to_id_str(username)}.json"
-    )
+    done = False
+    json_data = None
+    while not done:
+        try:
+            json_data = requests.get(
+                f"https://pokemonshowdown.com/users/{to_id_str(username)}.json"
+            )
+            done = True
+        except requests.exceptions.SSLError:
+            print("SSL Error...")
+            time.sleep(10)
+            print("Retrying...")
+    assert json_data is not None
     data = json.loads(json_data.content)
     rating_data = data["ratings"][battle_format]
     for key, value in rating_data.items():
