d87ff08d1440efe4b6fc2ff4dce86fab96c1d1ff
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-12-06 21:28:46 +0100

    Add license headers to all python files

--- tests/unit_tests/agents/test_scan_agent.py
+++ tests/unit_tests/agents/test_scan_agent.py
@@ -1,3 +1,20 @@
+#
+# A pokémon showdown battle-bot project based on reinforcement learning techniques.
+# Copyright (C) 2022 Matteo Dell'Acqua
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 from unittest.mock import patch, MagicMock
 
 from agents.utils.scan_agent import ScanAgent
--- utils/prettify_cross_evaluation.py
+++ utils/prettify_cross_evaluation.py
@@ -1,3 +1,20 @@
+#
+# A pokémon showdown battle-bot project based on reinforcement learning techniques.
+# Copyright (C) 2022 Matteo Dell'Acqua
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 # Prettifies a poke-env cross evaluation dictionary
 from tabulate import tabulate
 
